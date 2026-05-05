986b4346440fd6b275e09ad4f77cdc66eb5a2621
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-05-05 15:11:41 +0530

    Used loop to display items and changed some other too..

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ function btnClick(){
     let data = document.getElementById("text-box").value;
     text.push(data);
     document.getElementById("text-box").value="";
-};
+    display()
+}; function display(){
+    let list=document.createElement("li")
+   for(let i=0;i<text.length;i++){
+    list.innerHTML=text[i]
+    document.querySelector("ul").appendChild(list)
+   }
+}
